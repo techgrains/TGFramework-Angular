76111f7a5b6a6fe959551e7d841af58bd7013537
shailesh parmar <shailesh.parmar@techgrains.com>
2024-07-16 15:45:55 +0530

    #Angular version upgrade into angular 18.1.0

--- projects/core/src/lib/tgcore.module.ts
+++ projects/core/src/lib/tgcore.module.ts
@@ -1,23 +1,17 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { TGLoaderService, TGHttpClient, TGConfig } from './service';
 import { TGError } from './model';
 
 
-@NgModule({
-  declarations: [],
-  imports: [
-    FormsModule,
-    HttpClientModule,
-  ],
-  providers: [
-    TGLoaderService,
-    TGHttpClient
-  ],
-  exports: []
-})
+@NgModule({ declarations: [],
+    exports: [], imports: [FormsModule], providers: [
+        TGLoaderService,
+        TGHttpClient,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class TGCoreModule {
   static forRoot(config: TGConfig): ModuleWithProviders<TGCoreModule> {
     return {
