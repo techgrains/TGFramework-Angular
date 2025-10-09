342b73a6378d4922e4c9acbec15719e23cfd9898
Shailesh Parmar <shailesh.parmar@techgrains.com>
2025-10-09 12:28:43 +0530

    Update to angular version 20

--- projects/core/src/lib/singleton/tg-session-storage.singleton.ts
+++ projects/core/src/lib/singleton/tg-session-storage.singleton.ts
@@ -71,9 +71,9 @@ export class TGSessionStoarage {
      * Gets sting value for given key
      *
      * @param {string} key  Key of storage
-     * @returns {number}    Return value
+     * @returns {string | null}    Return value
      */
-    getString(key: string): string {
+    getString(key: string): string | null {
         const value = this.get(key);
         return this.isNull(value) ? null : this.get(key);
     }
@@ -113,9 +113,9 @@ export class TGSessionStoarage {
      * Gets object value for given key
      *
      * @param {string} key  Key of storage
-     * @returns {number}    Return value
+     * @returns {object | null}    Return value
      */
-    getObject(key: string): object {
+    getObject(key: string): object | null {
         const value = this.get(key);
         return this.isNull(value) ? null : JSON.parse(value);
     }
